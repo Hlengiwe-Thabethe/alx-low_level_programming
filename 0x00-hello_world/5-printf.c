e3d9d334d948f093ca6f12faf110738bdf9938b2
Hlengiwe-Thabethe <Hlengiwe-Thabethe>
2023-02-16 13:54:36 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line usin the printf function
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}	
