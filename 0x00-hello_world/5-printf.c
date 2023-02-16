61a3a7ab2227056ec08dc6762f1dd4e5e94b19e1
Hlengiwe-Thabethe <Hlengiwe-Thabethe>
2023-02-16 14:03:01 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - a program that prints a line usin the printf function
+ * main - A program that prints a line using the printf function
  * Return: 0 (Sucess)
  */
 int main(void)
