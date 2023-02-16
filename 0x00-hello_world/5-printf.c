9884e3dbb0e5cbbbdf22c6147d4af7004ed51dea
Hlengiwe-Thabethe <Hlengiwe-Thabethe>
2023-02-16 15:43:55 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," followed newline
+ * main - A program that prints a line using the printf function
  * Return: Always 0
  */
 int main(void)
