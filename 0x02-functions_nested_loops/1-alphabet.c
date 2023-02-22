631c97281e698f7f04ff2f997f4de1b817cf69ae
Hlengiwe-Thabethe <Hlengiwe-Thabethe>
2023-02-21 17:23:30 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
  * print_alphabet - print all alphabet in lowercase
  */
+
 void print_alphabet(void)
 {
 	char letters;
-	for (letter = 'a'; letter <= 'z'; letter ++)
-		_putchar(letter);
-	_putchar('\n');
-}
+	for(letter = 'a'; letter <= 'z'; letter++)
+		
+		_putchar('\n');
+}	
