810d92138ef2166a8eec5b8023a392582935f482
Hlengiwe-Thabethe <Hlengiwe-Thabethe>
2023-02-22 13:33:05 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,25 @@
 #include "main.h"
+
 /**
  * main - prints _putchar and a new line
+ *
  * return: Always 0
+ *
  */
+
 int main(void)
+
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	int num[] = {95, 112, 117, 116, 99, 104, 97, 114};
+
+	int index, size;
+
+	size = sizeof(nums) / sizeof(nums[index]);
+
+	for (index = 0; index < size; index++)
+	{
+		_putchar(nums[index]);
+	}
+	_putchar('\n');
+	return (0);
 }
