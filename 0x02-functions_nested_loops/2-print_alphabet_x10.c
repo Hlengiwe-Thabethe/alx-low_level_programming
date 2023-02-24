7ba29fc3098531e1940e07469c687dc1a9a02fcd
Hlengiwe-Thabethe <Hlengiwe-Thabethe>
2023-02-24 11:02:03 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints alpha 10 lines
+ * print_alphabet_x10 -> prints alpha 10 lines
  */
 
 void print_alphabet_x10(void)
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 {
 	int j;
 
-	int x;
+	int l;
 
-	for (x = 0; x <= 9; x++)
+	for (l = 0; l <= 9; l++)
 	{
 	for (j = 'a'; j <= 'z'; j++)
 		_putchar(j);
