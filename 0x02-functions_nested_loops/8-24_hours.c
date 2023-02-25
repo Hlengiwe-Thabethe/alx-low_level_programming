266d910599538c5c3d949974a66bc9ad88488e9e
Hlengiwe-Thabethe <Hlengiwe-Thabethe>
2023-02-24 17:20:10 -0800

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,34 +2,33 @@
 
 /**
  * jack_bauer -> prints 24 hours
+ * Return: 24 hours
  */
 
 void jack_bauer(void)
 
 {
-	int i, j;
+	int a, b, c, d;
 
-	for (i = 0; i <= 24; i++)
+	for (a = 0; a <= 2; a++)
 	{
-		for (j = 0; i <= 60; j++)
-		{
-			if (i < 10)
-			{
-				_putchar('0');
-				_putchar(i + '0');
-			}
-			if (j < 10)
-			{
-				_putchar('0');
-				_putchar(j + '0');
-			}
-			else if (j >= 10)
-			{
-				_putchar(':');
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-			}
-		}
-		_putchar('\n');
+	for (b = 0; b <= 9; b++)
+	{
+	if ((a <= 1 &&  b <= 9) || (a <= 2 && b <= 3))
+	{
+	for (c = 0; c <= 5; c++)
+	{
+	for (d = 0; d <= 9; d++)
+	{
+	_putchar(a + '0');
+	_putchar(b + '0');
+	_putchar(58);
+	_putchar(c + '0');
+	_putchar(d + '0');
+	_putchar('\n');
+	}
+	}
+	}
+	}
 	}
 }
