3681ca753f8d0ee5b087de8447ab423f1c7f8ace
Hlengiwe-Thabethe <Hlengiwe-Thabethe>
2023-02-25 05:08:22 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @a: The first number
+ * @b: The second number
+ * Return: The addition of both
+ */
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
