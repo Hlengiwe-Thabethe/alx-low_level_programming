a93670257bb8bfac8df83b3fa91a5548fd1d8da4
Hlengiwe-Thabethe <Hlengiwe-Thabethe>
2023-02-25 04:04:38 -0800

    hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,10 +9,10 @@ void print_alphabet(void)
 {
 	int c;
 
-	for
-		(c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-			_putchar('\n');
-		}
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
 }
