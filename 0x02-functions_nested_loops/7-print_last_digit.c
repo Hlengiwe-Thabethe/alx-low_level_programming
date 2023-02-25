9c36113d42ef8ff5d9d33c2607c09589db9e0253
Hlengiwe-Thabethe <Hlengiwe-Thabethe>
2023-02-25 04:30:01 -0800

    hot fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit -> prints last digit
  * @n: the passed argument
- * Return: the last digit
+ * Return: x
  */
 
 int print_last_digit(int n)
@@ -11,9 +11,11 @@ int print_last_digit(int n)
 {
 	int x;
 
-	if (n < 0)
-	n = -n;
 	x = n % 10;
+	if (x < 0)
+	{
+		x = x * -1;
+	}
 	_putchar(x + '0');
 	return (x);
 }
