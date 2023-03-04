850b508555d68ea2dc006a3baf73ca13600fac71
Hlengiwe-Thabethe <Hlengiwe-Thabethe>
2023-03-04 09:40:55 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '1'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
